b6151c400931f511c2e57bc795d263979cf3824d
ShivamRaje <shivamraje848@gmail.com>
2025-07-18 11:30:08 +0530

    Changes done

---
+++ DSA/Sorting/MergeSort.java
@@ -0,0 +1,39 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+
+public class MergeSort {
+    //perform sorting
+    private static void insertsort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key=arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+
+    }
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Enter array size : ");
+    int size = sc.nextInt();
+    System.out.println("Enter array elements : ");
+    int[] arr=new int[size];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i]=sc.nextInt();
+    }
+    insertsort(arr);
+    
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
+    }
+    sc.close();
+
+
+    }
+}
