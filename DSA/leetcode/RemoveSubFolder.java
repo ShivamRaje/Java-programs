6ba87cec10a20b453337b5d2e3234b71cda80870
ShivamRaje <shivamraje848@gmail.com>
2025-07-19 10:24:58 +0530

    Changes done

---
+++ DSA/leetcode/RemoveSubFolder.java
@@ -0,0 +1,39 @@
+package DSA.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoveSubFolder {
+    public static void removeSubFolder(String[] folder){
+        Arrays.sort(folder);
+        List<String> ans=new ArrayList<>();
+        ans.add(folder[0]);
+
+        for(int i=1;i<folder.length;i++){
+            int m=ans.get(ans.size()-1).length();
+            int n=folder[i].length();
+            if(m>=n
+            || !(ans.get(ans.size()-1).equals(folder[i].substring(0,m))
+            && folder[i].charAt(m)=='/')){
+                ans.add(folder[i]);
+            }
+        }
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of an array : ");
+        int size=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter folders: ");
+        String[] folder=new String[size];
+        for (int i = 0; i < folder.length; i++) {
+            folder[i]=sc.nextLine();
+        }
+        removeSubFolder(folder);
+        sc.close();
+    }
+}
