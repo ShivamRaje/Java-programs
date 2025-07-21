1f8b0b3f4486f4246c6a41c75965e352df3f9780
ShivamRaje <shivamraje848@gmail.com>
2025-07-21 10:23:36 +0530

    Changes done

---
+++ DSA/leetcode/RemoveChar.java
@@ -0,0 +1,33 @@
+package DSA.leetcode;
+
+import java.util.Scanner;
+
+public class RemoveChar {
+
+    public static String removeC(String s){
+        StringBuilder sb= new StringBuilder(s);
+        int count=1;
+        for (int i = 1; i <s.length(); i++) {
+            if (s.charAt(i)==s.charAt(i-1)) {
+                count++;
+            } else {
+                count=1;
+            }
+
+            if (count<3) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return s.length() > 0 ? s.charAt(0) + sb.toString() : "";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String s=sc.nextLine();
+        sc.nextLine();
+        String newS=removeC(s);
+        //System.out.println(newS);
+        sc.close();
+    }
+}
