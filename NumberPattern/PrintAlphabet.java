1acc69a87cd06ce75136ec9b09a5d198360464dc
ShivamRaje <shivamraje848@gmail.com>
2025-07-23 12:31:25 +0530

    Changes done

--- NumberPattern/PrintAlphabet.java
+++ NumberPattern/PrintAlphabet.java
@@ -2,7 +2,7 @@ public class PrintAlphabet{
     public static void main(String[] args) {
         char ch;
         for(ch='a';ch<='z';ch++){
-            System.out.print(ch+");
+            System.out.print(ch+" ");
         }
 
     }
