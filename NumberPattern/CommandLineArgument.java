93348a90fa347f1f8cecc4e130cabe8a14659f95
ShivamRaje <shivamraje848@gmail.com>
2025-07-23 12:27:38 +0530

    Changes done

---
+++ NumberPattern/CommandLineArgument.java
@@ -0,0 +1,9 @@
+
+public class CommandLineArgument {
+    public static void main(String[] args) {
+        for(String t:args){
+            System.out.println("CommandLine"+t);
+        }
+    }
+    
+}
