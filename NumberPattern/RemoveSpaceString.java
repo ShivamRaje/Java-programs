76519c5f78f3b6ffec77db83f24de898d21fb944
ShivamRaje <shivamraje848@gmail.com>
2025-07-26 18:06:10 +0530

    Changes done

--- NumberPattern/RemoveSpaceString.java
+++ NumberPattern/RemoveSpaceString.java
@@ -1,7 +1,7 @@
 public class RemoveSpaceString {
     public static void main(String[] args) {
         String s1="  hello string  ";
-        //without trim
+        //without trim.
         System.out.println(s1+"HI");
         //with trim()
         System.out.println(s1.trim()+"HI");
