f3067efae411dedc534b711a65159e8e37211801
ShivamRaje <shivamraje848@gmail.com>
2025-07-28 16:37:32 +0530

    Changes done

---
+++ NumberPattern/ConstructorOverloading.java
@@ -0,0 +1,13 @@
+public class ConstructorOverloading {
+    String name;
+    ConstructorOverloading(){
+        System.out.println("Constructor method called.");
+    }
+
+    ConstructorOverloading(String t){
+        name=t;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
