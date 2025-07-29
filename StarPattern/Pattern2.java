eb4f00c4ce4616d6855351e0054f4be412a19a6f
ShivamRaje <shivamraje848@gmail.com>
2025-07-29 20:49:33 +0530

    Changes done

--- StarPattern/Pattern2.java
+++ StarPattern/Pattern2.java
@@ -9,7 +9,7 @@ public class Pattern2 {
         int n=sc.nextInt();
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <= n ; j++) {
-                System.out.print(i+"");
+                System.out.print(i+" ");
             }
             System.out.println();
         }
