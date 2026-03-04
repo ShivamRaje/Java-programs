db1847167d8a8d549a4d41a55c87fd33ca5c48b9
Shivam Vinayak Raje <145598907+ShivamRaje@users.noreply.github.com>
2026-03-04 10:46:44 +0530

    Added Pattern6

---
+++ StarPattern/Pattern6.java
@@ -0,0 +1,28 @@
+/*
+1 2 3 4 5 
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25 
+*/ 
+import java.util.*;
+public class Pattern6
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number  : ");
+    int n=sc.nextInt();
+    int count=1;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=n;j++)
+      {
+        System.out.print(count+" ");
+        count++;
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
